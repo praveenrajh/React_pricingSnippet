a514b59df31eeaaeeef55ced1139df7625f83b5f
praveenrajh <ppcupcs@gmail.com>
2022-08-11 02:26:39 +0530

    Added some feautures

--- src/App.js
+++ src/App.js
@@ -1,72 +1,74 @@
-import './App.css';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
-import Cards from './cards';
-
-
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Cards from "./cards";
 
 function App() {
-
   let data = [
     {
       title: "Free",
-      price: "$0",
+      currency: "$",
+      price: 0,
+      period: "month",
       details: [
-        { list: "Single User", class: ""},
-        { list: "5GB Storage", class: "" },
-        { list: "Unlimited Public Projects", class: "" },
-        { list: "Community Access", class: "" },
-        { list: "Unlimited Private Projects", class: "text-muted" },
-        { list: "Dedicated Phone Support", class: "text-muted" },
-        { list: "Free Subdomain", class: "text-muted" },
-        { list: "Monthly Status Reports", class: "text-muted" },
-      ]
+        { list: "Single User", isEnable: true },
+        { list: "5GB Storage", isEnable: true },
+        { list: "Unlimited Public Projects", isEnable: true },
+        { list: "Community Access", isEnable: true },
+        { list: "Unlimited Private Projects", isEnable: false },
+        { list: "Dedicated Phone Support", isEnable: false },
+        { list: "Free Subdomain", isEnable: false },
+        { list: "Monthly Status Reports", isEnable: false },
+      ],
     },
-  
+
     {
       title: "Plus",
-      price: "$9",
+      currency: "$",
+      price: 9,
+      period: "month",
       details: [
-        { list: "5 User", class: ""},
-        { list: "50GB Storage", class: "" },
-        { list: "Unlimited Public Projects", class: "" },
-        { list: "Community Access", class: "" },
-        { list: "Unlimited Private Projects", class: "" },
-        { list: "Dedicated Phone Support", class: "" },
-        { list: "Free Subdomain", class: "" },
-        { list: "Monthly Status Reports", class: "text-muted" },
-      ]
+        { list: "5 User", isEnable: true, isBold: true },
+        { list: "50GB Storage", isEnable: true },
+        { list: "Unlimited Public Projects", isEnable: true },
+        { list: "Community Access", isEnable: true },
+        { list: "Unlimited Private Projects", isEnable: true },
+        { list: "Dedicated Phone Support", isEnable: true },
+        { list: "Free Subdomain", isEnable: true },
+        { list: "Monthly Status Reports", isEnable: false },
+      ],
     },
-  
+
     {
       title: "Pro",
-      price: "$49",
+      currency: "$",
+      price: 49,
+      period: "month",
       details: [
-        { list: "Unlimited User", class: ""},
-        { list: "150GB Storage", class: "" },
-        { list: "Unlimited Public Projects", class: "" },
-        { list: "Community Access", class: "" },
-        { list: "Unlimited Private Projects", class: "" },
-        { list: "Dedicated Phone Support", class: "" },
-        { list: "Unlimited Free Subdomain", class: "" },
-        { list: "Monthly Status Reports", class: "" },
-      ]
-    }
+        { list: "Unlimited User", isEnable: true, isBold: true },
+        { list: "150GB Storage", isEnable: true },
+        { list: "Unlimited Public Projects", isEnable: true },
+        { list: "Community Access", isEnable: true },
+        { list: "Unlimited Private Projects", isEnable: true },
+        { list: "Dedicated Phone Support", isEnable: true },
+        { list: "Unlimited Free Subdomain", isEnable: true },
+        { list: "Monthly Status Reports", isEnable: true },
+      ],
+    },
   ];
-  
+
   return (
     <section className="pricing py-5">
-       <div className="container">
-          <div className="row">
-            {data.map((data)=>{
-              return(
-                <Cards
-                title = {data.title}
-                price = {data.price}
-                details = {data.details}/>
-              )
-            })}
-          </div>
-       </div>
+      <div className="container">
+        <div className="row">
+          {data.map((item) => {
+            return (
+              <Cards
+                item= {item}
+              />
+            );
+          })}
+        </div>
+      </div>
     </section>
   );
 }
--- src/cards.js
+++ src/cards.js
@@ -1,31 +1,47 @@
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCheck, faTimes } from "@fortawesome/free-solid-svg-icons";
 
-function Cards(props) {
+function Cards({ item }) {
   return (
     <div className="col-lg-4">
-        <div className="card mb-5 mb-lg-0">
-          <div className="card-body">
-            <h5 className="card-title text-muted text-uppercase text-center">{props.title}</h5>
-            <h6 className="card-price text-center">{props.price}<span className="period">/month</span></h6>
-            <hr/>
-            <ul className="fa-ul">
-              <li className={props.details[0].class}>{props.details[0].list}</li>
-              <li className={props.details[1].class}>{props.details[1].list}</li>
-              <li className={props.details[2].class}>{props.details[2].list}</li>
-              <li className={props.details[3].class}>{props.details[3].list}</li>
-              <li className={props.details[4].class}>{props.details[4].list}</li>
-              <li className={props.details[5].class}>{props.details[5].list}</li>
-              <li className={props.details[6].class}>{props.details[6].list}</li>
-              <li className={props.details[7].class}>{props.details[7].list}</li>
-            </ul>
-            <div className="d-grid">
-              <a href="#" className="btn btn-primary text-uppercase">Button</a>
-            </div>
+      <div className="card mb-5 mb-lg-0">
+        <div className="card-body">
+          <h5 className="card-title text-muted text-uppercase text-center">
+            {item.title}
+          </h5>
+          <h6 className="card-price text-center">
+            {item.currency}
+            {item.price}
+            <span className="period">/{item.period}</span>
+          </h6>
+          <hr />
+          <ul className="fa-ul">
+            {item.details.map((terms) => {
+              return (
+                <li className={terms.isEnable ? `` : `text-muted`}>
+                  <span className="fa-li">
+                    {terms.isEnable ? (
+                      <FontAwesomeIcon icon={faCheck} />
+                    ) : (
+                      <FontAwesomeIcon icon={faTimes} />
+                    )}
+                  </span>
+                  <span>
+                    {terms.isBold ? <strong>{terms.list}</strong> : terms.list}
+                  </span>
+                </li>
+              );
+            })}
+          </ul>
+          <div className="d-grid">
+            <a href="#" className="btn btn-primary text-uppercase">
+              Button
+            </a>
           </div>
         </div>
       </div>
-      
-  )
+    </div>
+  );
 }
 
 export default Cards;
